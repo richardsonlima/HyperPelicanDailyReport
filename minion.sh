3a4ea82eeea2fbdae9b8735ea235586cdeb9d008
Richardson Lima <contatorichardsonlima@gmail.com>
2015-01-15 00:58:01 -0300

    Update minion.sh

--- minion.sh
+++ minion.sh
@@ -44,7 +44,7 @@ cat << HEAD
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-xxxxxxxx', 'blog.richardsonlima.com.br');
+  ga('create', 'UA-48988720-3', 'auto');
   ga('send', 'pageview');
 
 </script>
